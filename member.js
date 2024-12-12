8598b8b78dbc23e006603dc416c296d8492bdd4c
dai <dosada@gmail.com>
2024-12-12 06:50:23 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,4 +1,4 @@
 function skillsmember() {
-    var skills = ["HTML", "CSS", "JavaScript"];
+    var skills = ["HTML", "CSS", "JavaScript", "Python", "Java", "C++", "C#", "Ruby", "PHP", "SQL"];
     return skills;
 }
